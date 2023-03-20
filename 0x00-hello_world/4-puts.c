5e69cac8deda58df7ee0d89f50de5310ad5232e2
hajar baskad <hajar.bask@gmail.com>
2023-03-20 04:07:13 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - A program that prints a line with puts function
- * return: 0 (Success)
- * /
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("\"Programming is like building multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
